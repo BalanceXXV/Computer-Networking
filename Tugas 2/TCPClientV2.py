8ccdc836674a1ee8045a71bad151aa065cc2c24e
Imbang Murtito <imbangmurtito@gmail.com>
2018-10-04 14:05:12 +0700

    change client

--- Tugas 2/TCPClientV2.py
+++ Tugas 2/TCPClientV2.py
@@ -1,10 +1,24 @@
 from socket import *
+import argparse
+
 serverName = 'localhost'
-serverPort = 23
+serverPort = 8023
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
-sentence = input('Input lowercase sentence:')
-clientSocket.send(sentence.encode('utf-8'))
+
+parser = argparse.ArgumentParser(description="List of server command")
+parser.add_argument('-c', help="Get server cpu info", action='store_const', const='cpuinfo')
+parser.add_argument('-m', help='Get server physical memory info', action='store_const', const='meminfo')
+parser.add_argument('-sm', help='Get server virtual memory info', action='store_const', const='memswap')
+parser.add_argument('-st', help='Get server file storage info', action='store_const', const='storage')
+parser.add_argument('-i', help="Check server internet connection status", action='store_const', const='internet')
+args = parser.parse_args()
+request = ''
+for key, value in vars(args).items():
+    if isinstance(value, str):
+        request += value + ' '
+
+clientSocket.send(request.encode('utf-8'))
 modifiedSentence = clientSocket.recv(1024)
 print (modifiedSentence.decode('utf-8'))
 clientSocket.close()
